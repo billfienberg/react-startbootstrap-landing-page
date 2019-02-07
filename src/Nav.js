1ba1252f56c6d36df174051d8e7600a7b4857ca6
Bill Fienberg <billfienberg@users.noreply.github.com>
2019-02-07 00:42:49 -0600

    Nav.js: Start using hooks

--- src/Nav.js
+++ src/Nav.js
@@ -1,22 +1,100 @@
-import React from "react"
+import React, { useState, Fragment } from "react"
+import Modal, { Header, Body, Footer } from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
 
-const Nav = ({ brandName = "" }) => {
+function LogInForm() {
   return (
-    <nav className="navbar navbar-light bg-light static-top">
-      <div className="container">
-        <a className="navbar-brand" href="/">
-          {brandName}
-        </a>
-        <div>
-          <button type="button" className="mx-1 btn btn-outline-secondary">
-            Log In
-          </button>
-          <button type="button" className="mx-1 btn btn-primary">
-            Sign Up
-          </button>
-        </div>
+    <form className="form-signin">
+      <div className="form-label-group">
+        <input
+          type="email"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          required
+          autofocus
+        />
+        <label htmlFor="inputEmail">Email address</label>
       </div>
-    </nav>
+
+      <div className="form-label-group">
+        <input
+          type="password"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          required
+        />
+        <label htmlFor="inputPassword">Password</label>
+      </div>
+
+      <div className="custom-control custom-checkbox mb-3">
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customCheck1"
+        />
+        <label className="custom-control-label" htmlFor="customCheck1">
+          Remember password
+        </label>
+      </div>
+      <button
+        className="btn btn-lg btn-primary btn-block text-uppercase"
+        type="submit"
+      >
+        Log in
+      </button>
+    </form>
+  )
+}
+
+function Nav({ brandName = "" }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const modal = isOpen && (
+    <Modal show={isOpen} onHide={() => setIsOpen(false)}>
+      <Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Header>
+      <Body>
+        <LogInForm />
+      </Body>
+      <Footer>
+        <Button variant="secondary" onClick={() => setIsOpen(false)}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={() => setIsOpen(false)}>
+          Save Changes
+        </Button>
+      </Footer>
+    </Modal>
+  )
+  return (
+    <Fragment>
+      {modal}
+      <nav className="navbar navbar-light bg-light static-top">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            {brandName}
+          </a>
+          <div>
+            <button
+              type="button"
+              className="mx-1 btn btn-outline-secondary"
+              onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+            >
+              Log In
+            </button>
+            <button
+              type="button"
+              className="mx-1 btn btn-primary"
+              onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
   )
 }
 
